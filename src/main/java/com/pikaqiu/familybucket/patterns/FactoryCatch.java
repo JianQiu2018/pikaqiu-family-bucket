6b11c8ee3490142ce1cd680f71f8d47699c3c0d3
qiujian <18296158343@163.com>
2021-01-04 10:51:41 +0800

    Test oss

--- src/main/java/com/pikaqiu/familybucket/patterns/FactoryCatch.java
+++ src/main/java/com/pikaqiu/familybucket/patterns/FactoryCatch.java
@@ -1,9 +1,16 @@
 package com.pikaqiu.familybucket.patterns;
 
+import com.alibaba.fastjson.JSONObject;
+import com.pikaqiu.familybucket.dto.AuthUserDTO;
 import com.pikaqiu.familybucket.patterns.decorator.ComponentCatch;
 import com.pikaqiu.familybucket.patterns.decorator.FirstCatch;
 import com.pikaqiu.familybucket.patterns.decorator.SecondCatch;
 import com.pikaqiu.familybucket.patterns.decorator.ThiredCatch;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 /**
  * 装饰模式和代理模式区别？
@@ -20,13 +27,61 @@ public class FactoryCatch {
         return thiredCatch;
     }
 
+    public static final String escapeHtml4(String str) {
+        return StringEscapeUtils.escapeHtml4(str);
+    }
+
     public static void main(String[] args) {
-        ComponentCatch getComponentCatch = getComponentCatch();
+        /*ComponentCatch getComponentCatch = getComponentCatch();
         Object value1 = getComponentCatch.getCatch("1");
         System.out.println("value1:" + value1);
         System.out.println("###########################");
         Object value2 = getComponentCatch.getCatch("1");
-        System.out.println("value2:" + value2);
+        System.out.println("value2:" + value2);*/
+
+        /*String str = "thi is a test <dsd,dsdd>这是[fdfd]一>个)&^测12232试18.21dsdd";
+        System.out.println("用escapeJava方法转义之后的字符串为:"
+                + StringEscapeUtils.escapeHtml3(str));
+
+        String files = "dsdss.xlsx";
+        System.out.println(files.substring(files.lastIndexOf(".")));*/
+
+
+
+        /*authUserDTO = JSONObject.parseObject(filterStr, AuthUserDTO.class);
+        System.out.println("---: " + authUserDTO.toString())*/;
+
+        AuthUserDTO authUserDTO = new AuthUserDTO();
+        authUserDTO.setPassword("税务受理,缴款中,增量/房业务,买卖,业务号:23301012787-20201111101420,坐落:未来科技城150号");
+        authUserDTO.setUserName("18296158343<script>'al'ert(1)</script>");
+        authUserDTO.setPhoneNumber("123,12323");
+
+        String str = JSONObject.toJSONString(authUserDTO);
+        System.out.println("Str: " + str);
+
+//
+        String str2 = escapeHtml4(str);
+        System.out.println(str2);
+
+        String filterStr = StringFilter(str2);
+        System.out.println(filterStr);
+
+
     }
+
+    /**
+     * 过滤特殊字符
+     * @param str
+     * @return
+     * @throws PatternSyntaxException
+     */
+    public static String StringFilter(String str) {
+//        String regEx="[`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+        String regEx="[\\\\`~!@#$%^&*()+=|';'<>?~！@#￥%……&*（）——+|【】‘；：”“’。，、？]";
+        Pattern p = Pattern.compile(regEx);
+        Matcher m = p.matcher(str);
+        return m.replaceAll("").trim();
+    }
+
 
 }
